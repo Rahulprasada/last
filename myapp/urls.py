b3323599b965cb02b334d7f158872145186553c1
Rahulprasada <rahulprasadkpm@gmail.com>
2024-12-10 18:27:21 +0530

    Fix static.py for Vercel

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.first, name = 'one'),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('signalview/', views.sigview, name='signalview'),
     path('stockview/', views.stockview, name="stockview"),
     path('fetch_filtered_data/', views.fetch_filtered_data, name='fetch_filtered_data'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
