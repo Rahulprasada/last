f25b83017340f77b545a9dc8c32fc52dd4326c1f
Rahulprasada <rahulprasadkpm@gmail.com>
2024-12-11 20:01:24 +0530

    Update wsgi.py for Vercel compatibility

--- Miststreet/wsgi.py
+++ Miststreet/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application # type: ignore
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Miststreet.settings')
 
 application = get_wsgi_application()
+
+# Alias the WSGI application as 'app' for Vercel compatibility
+app = application
